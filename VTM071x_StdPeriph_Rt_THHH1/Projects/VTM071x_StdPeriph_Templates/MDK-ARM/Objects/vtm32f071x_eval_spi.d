0c7479b841f8b4f69ce4f480efb062a05ce36538
YunLinkGitDemo <104413942+YunLinkGitDemo@users.noreply.github.com>
2022-05-10 17:59:43 +0800

    Delete VTM071x_StdPeriph_Rt_THHH1 directory

--- VTM071x_StdPeriph_Rt_THHH1/Projects/VTM071x_StdPeriph_Templates/MDK-ARM/Objects/vtm32f071x_eval_spi.d
+++
@@ -1,3 +0,0 @@
-.\objects\vtm32f071x_eval_spi.o: ..\..\..\Utilities\VTM32F071x_EVAL\Common\VTM32F071x_eval_spi.c
-.\objects\vtm32f071x_eval_spi.o: ..\..\..\Utilities\VTM32F071x_EVAL\Common\VTM32F071x_eval_spi.h
-.\objects\vtm32f071x_eval_spi.o: ..\..\..\Utilities\VTM32F071x_EVAL\VTM32F071x_eval.h
--- VTM071x_StdPeriph_Rt_THHH1/Projects/VTM071x_StdPeriph_Templates_RTT/MDK-ARM/Objects/startup_vtm071x.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_vtm071x.o: ..\..\..\Libraries\CMSIS\Device\MIC\VTM071x\Source\Templates\ARM\startup_VTM071x.s
